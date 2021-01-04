12c0436c421a9ecf2d62a84e24aef818da48301d
Shae Iswar <shae.iswar@gmail.com>
2021-01-05 11:41:12 +1300

    Modified user post links multiple templates 	modified:   flask_web/routes.py 	modified:   flask_web/site.db 	new file:   flask_web/static/title.jpg 	modified:   flask_web/templates/game.html 	modified:   flask_web/templates/layout.html 	modified:   flask_web/templates/post.html 	new file:   flask_web/templates/user_posts.html

--- flask_web/routes.py
+++ flask_web/routes.py
@@ -55,7 +55,8 @@ categories = [
 @app.route("/")
 @app.route("/game")
 def game():
-    posts = Post.query.all()
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('game.html', posts=posts, categories=categories)
 
 @app.route("/home")
@@ -187,3 +188,12 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('game'))
+
+@app.route("/user/<str:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = PPost.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
